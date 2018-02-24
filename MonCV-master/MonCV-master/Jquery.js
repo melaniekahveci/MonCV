f8a39a335839227657f6cdd22106f319ffd7768b
melaniekahveci <33230424+melaniekahveci@users.noreply.github.com>
2018-02-24 16:07:11 +0100

    Delete Jquery.js

--- MonCV-master/MonCV-master/Jquery.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('.flip h2').click(function(){
-        $this = $(this).parent('.flip');
-        $this.find('.panel').slideToggle("slow");
-        $this.toggleClass('hidden');
-    });
-});
-
